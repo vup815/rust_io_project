249b84fe85e06ecff68fd99c7d2e452bb6b4fb17
vup815@gmail <vup815@gmail.com>
2023-10-27 23:32:13 +0800

    feature(main): Extract logic from main and return error with `?`

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -10,10 +11,17 @@ fn main() {
         process::exit(1);
     });
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
