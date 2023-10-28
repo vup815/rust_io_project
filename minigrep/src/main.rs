fb3c92ae0cda98ee00b1cf4b3b02e8a45f6260ac
vup815@gmail <vup815@gmail.com>
2023-10-28 11:41:54 +0800

    feature(main): Write error msg to stderr with `eprintln!` macro

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arugments: {err}");
+        eprintln!("Problem parsing arugments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
